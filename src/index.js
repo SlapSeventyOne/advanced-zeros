287b034961e47563612523ac191c0c4477e498e5
Nikolay B <exedystyle@tutamail.com>
2019-02-27 01:38:40 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  let res = [];
+  for (let i = 2; i <= base; i++) { //search all primes that divide base
+    if (base % i == 0){
+      let power = 0; sum = 0; temp = number;
+        while (base % i == 0) {
+          power++;
+          base /= i;
+          }
+        while (temp / i > 0) {
+          temp = Math.floor(temp / i);
+          sum += temp;
+          }
+        res.push(Math.floor(sum / power));
+      }
+    }
+  return Math.min.apply(Math, res);
+  }
